c0dfabb84d598d01a30d160553819e65f0d6fc86
Deepak Choudhary <deepak.choudhary@niyotail.com>
2022-03-28 14:14:29 +0530

    forced update changes

--- revalidate.sh
+++ revalidate.sh
@@ -1,4 +1,9 @@
 curl "https://nextjs-pokemon-modes.vercel.app/api/revalidate"
     -X POST \
     -H "content-type:application/json" \
-    -d "[\"/pokemon/1"]"
+    -d "[\"/pokemon/1"]" 
+
+
+
+
+# "To run this, in terminal type,"     sh ./revalidate.sh   "and click enter."
